b40f1d891f95f20d1e2e8fb06e2ec75647fd4d9b
Kok3995 <kingofknights3995@gmail.com>
2021-07-24 19:02:36 +0700

    connect port 5556 bluestack

--- MacrorifyServiceInstaller/Connecter/ConnecterBlue.cs
+++ MacrorifyServiceInstaller/Connecter/ConnecterBlue.cs
@@ -14,11 +14,21 @@ namespace MacrorifyServiceInstaller
         public void Connect()
         {
             for (var i = 0; i <= 100; i += 10)
-            {
+            {             
                 try
                 {
                     AdbHelper.GetClient().Connect(new System.Net.DnsEndPoint(Constant.LOCALHOST, Constant.BLUE_PORT + i));
                 } catch { /*ignored*/ }
+
+                if (i == 0)
+                {
+                    try
+                    {
+                        //Also connect port 5556
+                        AdbHelper.GetClient().Connect(new System.Net.DnsEndPoint(Constant.LOCALHOST, Constant.BLUE_PORT + 1));
+                    }
+                    catch { /*ignored*/ }
+                }
             }
         }
     }
